c8d8eb87dc325244eb12161c879bfc95255d31ba
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2021-03-21 01:47:46 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,14 @@
+def say_hello_ten_times
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
---
+++ learning-method-scope.rb
@@ -0,0 +1,3 @@
+p SHARK_FAMILY
+["Baby Shark", "Mama Shark", "Papa Shark", "Grandma Shark", "Grandpa Shark"]
+ 
